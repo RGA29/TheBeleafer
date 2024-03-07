fce06c617428e2303b75f30ec5297876acbfcdea
Reem <abiammar.reem@gmail.com>
2024-03-06 23:45:24 -0300

    moved state higher to App and its working successfully

--- src/App.jsx
+++ src/App.jsx
@@ -4,15 +4,67 @@ import NavBar from './components/NavBar';
 
 // import './stylesheets/styles.css';
 
-
-const App = props => {
-    return (
-        <div className='appContainer'> 
-        <NavBar/>
-        <Products/>
+class App extends Component {
+    constructor(props) {
+        super(props); 
+        this.state = {
+            fetchedProd: false, 
+            products: [],
+            modalState: {
+                open: false, 
+                type: null, 
+                position: { top:0, left:0 }, 
+                id: null
+            }
+        };
         
+        this.openModal = this.openModal.bind(this); 
+        this.closeModal = this.closeModal.bind(this);
+    }
+
+    componentDidMount() {
+        fetch('/api/')
+        .then(res => res.json())
+        .then((products)=> {
+            if(!Array.isArray(products)) products = []; 
+            return this.setState({
+                products,
+                fetchedProd: true
+            });
+        })
+        .catch(err => console.log('Products.componentDidMount: get characters: ERROR: ', err))
+    }
+
+    openModal(type, position, id) {
+        this.setState({
+          modalState: {
+            ...this.state.modalState,
+            open: true,
+            type,
+            position,
+            id
+          }
+        });
+      }
+
+      closeModal() {
+        this.setState({
+          modalState: {
+            ...this.state.modalState,
+            open: false
+          }
+        });
+      }
+
+    render (){
+        return (
+            <div className='appContainer'> 
+            <NavBar/>
+            <Products products= {this.state.products}/>
         </div>
-    );
-};
+            
+        )
+    }
+}
 
 export default App; 
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -2,44 +2,10 @@ import React, {Component} from 'react';
 
 import ProductCard from './ProductCard';
 
-class Products extends Component {
-    constructor(props) {
-        super(props); 
-        this.state = {
-            fetchedProd: false, 
-            products: [],
-            // modalState: {
-            //     open: false, 
-            //     type: null, 
-            //     position: { top:0, left:0 }, 
-            //     id: null
-            // }
-        };
-        
-        // this.openModal = this.openModal.bind(this); 
-        // this.closeModal = this.closeModal.bind(this);
-    }
+const Products =(props) => {
 
-    componentDidMount() {
-        fetch('/api/')
-        .then(res => res.json())
-        .then((products)=> {
-            if(!Array.isArray(products)) products = []; 
-            return this.setState({
-                products,
-                fetchedProd: true
-            });
-        })
-        .catch(err => console.log('Products.componentDidMount: get characters: ERROR: ', err))
-    }
-
-    render (){
-        // if (!this.state.fetchedProd) return (
-        //     <div>
-        //         <h1>Loading data, please wait..</h1>
-        //     </div>
-        // )
-        const { products } = this.state; 
+    const { products } = props;
+    console.log("show me", products); 
         if (!products) return null; 
         if (!products.length) return (
         <div><h1>Sorry, no products found...</h1></div>
@@ -52,20 +18,20 @@ class Products extends Component {
             />
             );
         });
+    return (
+        <section className='mainSection'>
+        <header className="pageHeader">
+            <h2>Find your next succulent, herb or tree</h2>
+        </header>
+        <div className='prodContainer'>
+            {prodElems}
+        </div>
+    
+    </section>
 
-        return (
-            <section className='mainSection'>
-                <header className="pageHeader">
-                    <h2>Find your next succulent, herb or tree</h2>
-                </header>
-                <div className='prodContainer'>
-                    {prodElems}
-                </div>
+    )
 
-            </section>
-            
-        )
-    }
+
 }
 
 export default Products; 
